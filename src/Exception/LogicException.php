7326d80801d57f0fccac913191fb28199dfecd0c
Hannes Forsgård <hannes.forsgard@fripost.org>
2018-04-12 16:06:04 +0200

    Update exception class hierarcy

--- src/Exception/LogicException.php
+++ src/Exception/LogicException.php
@@ -3,7 +3,7 @@
 namespace byrokrat\banking\Exception;
 
 /**
- * Error in pakage logic, this exception should never be thrown
+ * Error in package logic, this exception should never be thrown
  */
 class LogicException extends \LogicException implements \byrokrat\banking\Exception
 {
